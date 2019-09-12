f19dc630c0b57a1aaad5519a25b09fcceecfdd4d
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-09-12 11:39:19 +0900

    実機でセーブデータが保存されていなかったので、データのパスを変更

--- Scripts/Data/JsonDataSaver.cs
+++ Scripts/Data/JsonDataSaver.cs
@@ -9,7 +9,23 @@ namespace VMUnityLib
 {
     public static class JsonDataSaver
     {
-        const string DataFolderPath = "/MyGameAssets/SaveData/";    // セーブデータ保存先パス
+        /// <summary>
+        /// データのパスを生成
+        /// </summary>
+        static string CreateDataPath<T>(T data)
+            where T : class
+        {
+            string dataName = data.ToString() + ".json";
+
+            string dataPath =
+#if UNITY_EDITOR
+            Application.dataPath + "/SaveData/" + dataName;
+#else
+            Application.persistentDataPath + dataName;
+#endif
+
+            return dataPath;
+        }
 
         /// <summary>
         /// セーブ
@@ -23,10 +39,9 @@ namespace VMUnityLib
 
             // データをJSON形式に変換
             string jsonStr = JsonUtility.ToJson(saveData);
-            string dataName = saveData.ToString() + ".json";
 
             // JSONファイルを作成し、書き込み（クラスの名前のJSONファイルを作成）
-            writer = new StreamWriter(Application.dataPath + DataFolderPath + dataName, false);
+            writer = new StreamWriter(CreateDataPath(saveData), false);
             writer.Write(jsonStr);
             writer.Flush();
             writer.Close();
@@ -44,10 +59,9 @@ namespace VMUnityLib
 
             // 読み込んだJSONファイルを保存する変数を用意
             string dataStr = "";
-            string dataName = loadData.ToString() + ".json";
 
             // JSONファイルを読み込み、string型で保存
-            reader = new StreamReader(Application.dataPath + DataFolderPath + dataName);
+            reader = new StreamReader(CreateDataPath(loadData));
             dataStr = reader.ReadToEnd();
             reader.Close();
 
@@ -63,12 +77,8 @@ namespace VMUnityLib
         public static bool FileExists<T>(T checkData)
             where T : class
         {
-            // クラス名からファイルパスを作成
-            string dataName = checkData.ToString() + ".json";
-            string dataPath = Application.dataPath + DataFolderPath + dataName;
-
             // 指定したファイルがあればtrueを返す
-            if (File.Exists(dataPath))
+            if (File.Exists(CreateDataPath(checkData)))
             {
                 return true;
             }
