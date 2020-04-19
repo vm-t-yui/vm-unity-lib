d40cf3edec71fc9be2f98fe66d5a8efa9e4fbe09
Takeru Yui <t.yui@tri-core.co.jp>
2020-04-19 12:26:38 +0900

    不要になったプレハブ削除 transform拡張追加

---
+++ Scripts/Utility/TransformExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Transformの拡張クラス
+/// </summary>
+public static class TransformExtension
+{
+    /// <summary>
+    /// position、rotation、scaleをリセットする
+    /// </summary>
+    public static void Reset(this Transform transform)
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+    }
+
+}
