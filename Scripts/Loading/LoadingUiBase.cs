e9c896ff2525f9d547632f17cf4b73a6211b96d6
tricore-t-mitsumaru <t.mitsumaru@tri-core.co.jp>
2020-06-11 12:58:54 +0900

    指摘箇所の修正

--- Scripts/Loading/LoadingUiBase.cs
+++ Scripts/Loading/LoadingUiBase.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
-using Sirenix.OdinInspector;
 
-/// <summary>
-/// ロードUIのベースクラス
-/// </summary>
-[TypeInfoBox("ロードUIのオブジェクトには、このクラスを必ずアタッチしてください。\n" +
-             "特定のロードUIで固有の処理が必要な場合はこのクラスを継承してください。")]
-public class LoadingUiBase : MonoBehaviour
+namespace VMUnityLib
 {
-    [SerializeField,LabelText("UIの処理が終了したかどうか")]
-    [InfoBox("ロードUIの処理で次のシーンへ移行できるタイミングになったときにisEndをtrueにしてください。\n" +
-             "trueになったら自動でロードUIが閉じられ、次のシーンへ移行します。")]
-    protected bool isEnd = false;
-    public bool IsEnd => isEnd;
+    /// <summary>
+    /// ロードUIのベースクラス
+    /// NOTE : t-mitsumaru ロードUIのオブジェクトには、このクラスを必ずアタッチしてください。
+    ///                    特定のロードUIで固有の処理が必要な場合はこのクラスを継承してください。
+    /// </summary>
+    public class LoadingUiBase : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("ロードUIの処理で次のシーンへ移行できるタイミングになったときにisEndをtrueにしてください。\n" +
+                 "trueになったら自動でロードUIが閉じられ、次のシーンへ移行します。")]
+        protected bool isEnd = false;
+        public bool IsEnd => isEnd;
+    }
 }
