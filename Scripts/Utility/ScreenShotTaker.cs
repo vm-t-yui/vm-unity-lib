bba16ae17c973e2e86d113a1ceb2f3e5db0ebca1
vm-t-yui <t.yui@tri-core.co.jp>
2023-12-27 10:11:12 +0900

    どこでもスクショ取れる機能を追加

---
+++ Scripts/Utility/ScreenShotTaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace VMUnityLib
+{
+    /// <summary>
+    /// シーン名を定数で管理するクラスを作成するスクリプト
+    /// </summary>
+    public static class ScreenShotTaker
+    {
+        const string ITEM_NAME  = "Tools/Game画面解像度でScreenShotを撮る";    // コマンド名
+        [MenuItem(ITEM_NAME)]
+        public static void TakeScreenShot()
+        {
+            string desktopDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(desktopDirectoryPath, "screenshot_" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + ".png"));
+        }
+    }
+}
