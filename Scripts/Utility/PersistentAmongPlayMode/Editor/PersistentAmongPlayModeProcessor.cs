e67eb1e5dcf1e89bd28b6eb21e06b38500c22a93
vm-t-yui <t.yui@tri-core.co.jp>
2024-05-24 16:22:07 +0900

    実行中、ゲームオブジェクトの右クリックメニューにTransformを保存できる仕組みを実装

--- Scripts/Utility/PersistentAmongPlayMode/Editor/PersistentAmongPlayModeProcessor.cs
+++ Scripts/Utility/PersistentAmongPlayMode/Editor/PersistentAmongPlayModeProcessor.cs
@@ -18,9 +18,20 @@ using Object = UnityEngine.Object;
 [InitializeOnLoad] //エディター起動時に初期化されるように
 public class PersistentAmongPlayModeProcessor
 {
-
     //エディタ停止直前の値を記録するためのDict(InstanceIDとフィールド名をKeyにし、その値を設定する感じ)
     private static readonly Dictionary<int, Dictionary<string, object>> _valueDictDict = new Dictionary<int, Dictionary<string, object>>();
+
+    // トランスフォーム保存用データ
+    struct TransformData
+    {
+        public Vector3 pos;
+        public Quaternion rot;
+        public Vector3 scale;
+    }
+
+    // エディタ停止直前のTransformを記録するためのDict。キーはグローバルオブジェクトのID
+    private static readonly Dictionary<string, TransformData> _transformDataDict = new Dictionary<string, TransformData>();
+    private static readonly Dictionary<string, Transform>     _transformDict = new Dictionary<string, Transform>();
 
     //=================================================================================
     //初期化
@@ -37,11 +48,19 @@ public class PersistentAmongPlayModeProcessor
             {
                 _valueDictDict.Clear();
                 ExecuteProcessToAllMonoBehaviour(SaveValue);
+
+                // transform適用
+                SaveTransformValue();
+                _transformDict.Clear();
             }
             //実際に終了した時(シーン再生前の値に戻った時)に、保存してた値を反映
             else if (state == PlayModeStateChange.EnteredEditMode)
             {
                 ExecuteProcessToAllMonoBehaviour(ApplyValue);
+
+                // transform適用
+                ApplyTransformValue();
+                _transformDataDict.Clear();
             }
         };
 
@@ -96,6 +115,57 @@ public class PersistentAmongPlayModeProcessor
         _valueDictDict.Add(component.GetInstanceID(), valueDict);
     }
 
+    //トランスフォームを実行中に保存
+    [MenuItem("CONTEXT/Transform/実行中のTransformを終了後も保持")]
+    private static void AddTransform(MenuCommand menuCommand)
+    {
+        // 実行中以外は無視
+        if (!EditorApplication.isPlaying)
+        {
+            return;
+        }
+
+        var target = menuCommand.context as Transform;
+
+        var id = GlobalObjectId.GetGlobalObjectIdSlow(target).ToString();
+        if (!_transformDict.ContainsKey(id))
+        {
+            _transformDataDict.Add(id, new TransformData());
+            _transformDict.Add(id, target);
+        }
+    }
+
+    /// <summary>
+    /// 保存
+    /// </summary>
+    private static void SaveTransformValue()
+    {
+        if(_transformDict.Count == 0)
+        {
+            return;
+        }
+
+        var sceneObjects = GameObject.FindObjectsOfType<Transform>();
+        var sceneObjectIds = new GlobalObjectId[sceneObjects.Length];
+        GlobalObjectId.GetGlobalObjectIdsSlow(sceneObjects, sceneObjectIds);
+
+        for (var i = 0; i < sceneObjectIds.Length; i++)
+        {
+            var idString = sceneObjectIds[i].ToString();
+            var target = sceneObjects[i];
+            if (_transformDict.ContainsKey(idString))
+            {
+                TransformData targetTransformData = new TransformData();
+
+                targetTransformData.pos = target.position;
+                targetTransformData.scale = target.localScale;
+                targetTransformData.rot = target.rotation;
+
+                _transformDataDict[idString] = targetTransformData;
+            }
+        }
+    }
+
     //=================================================================================
     //反映
     //=================================================================================
@@ -125,13 +195,61 @@ public class PersistentAmongPlayModeProcessor
                     var dictValue = valueDict[fieldName];
                     if (value != null && dictValue != null)
                     {
-                        isChangedValue = isChangedValue || value.ToString() != dictValue.ToString();
+                        var changed = value.ToString() != dictValue.ToString();
 
                         //値の反映
-                        fieldInfo.SetValue(component, valueDict[fieldName]);
+                        if(changed)
+                        {
+                            fieldInfo.SetValue(component, valueDict[fieldName]);
+                            isChangedValue = true;
+                        }
                     }
                 }
         });
+
+        //値の変更があったら保存出来るようにするため、シーンに変更があったこと(米印)を設定
+        if (isChangedValue)
+        {
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+    }
+
+    /// <summary>
+    /// Transformの反映
+    /// </summary>
+    private static void ApplyTransformValue()
+    {
+        if(_transformDataDict.Count == 0)
+        {
+            return;
+        }
+
+        //PersistentAmongPlayModeの属性が付いた値だけ反映
+        var isChangedValue = false; //値に変更があったか
+
+        var sceneObjects = GameObject.FindObjectsOfType<Transform>();
+        var sceneObjectIds = new GlobalObjectId[sceneObjects.Length];
+        GlobalObjectId.GetGlobalObjectIdsSlow(sceneObjects, sceneObjectIds);
+
+        for (var i = 0; i < sceneObjectIds.Length; i++)
+        {
+            var idString = sceneObjectIds[i].ToString();
+            var target = sceneObjects[i];
+            if (_transformDataDict.ContainsKey(idString))
+            {
+                var value = _transformDataDict[idString];
+
+                if(target.position != value.pos
+                    || target.localScale != value.scale
+                    || target.rotation != value.rot)
+                {
+                    target.position = value.pos;
+                    target.localScale = value.scale;
+                    target.rotation = value.rot;
+                    isChangedValue = true;
+                }
+            }
+        }
 
         //値の変更があったら保存出来るようにするため、シーンに変更があったこと(米印)を設定
         if (isChangedValue)
