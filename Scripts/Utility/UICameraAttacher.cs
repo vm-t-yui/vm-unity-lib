7e04871a509f694913a22f88b8990d75ad5c40af
vm-t-yui <t.yui@tri-core.co.jp>
2025-03-26 17:12:15 +0900

    カメラをスタックする機能をattacherに追加

--- Scripts/Utility/UICameraAttacher.cs
+++ Scripts/Utility/UICameraAttacher.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 /// <summary>
 /// 指定されたCanvasにUI用のCameraをアタッチする
@@ -8,9 +7,35 @@ using UnityEngine;
 public class UICameraAttacher : MonoBehaviour
 {
     [SerializeField] Canvas targetCanvas = default;
+    [SerializeField] Camera targetCamera = default;
     private void Awake()
     {
-        targetCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-        targetCanvas.worldCamera = CommonUiRoot.Inst.UiCamera;
+        if (targetCanvas)
+        {
+            targetCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+            targetCanvas.worldCamera = CommonUiRoot.Inst.UiCamera;
+        }
+        if(targetCamera)
+        {
+            // プレイヤーのカメラにUIカメラをアタッチ
+            var baseCameraData = targetCamera.GetUniversalAdditionalCameraData();
+            var overlayCameraData = CommonUiRoot.Inst.UiCamera.GetUniversalAdditionalCameraData();
+            if (!baseCameraData.cameraStack.Contains(CommonUiRoot.Inst.UiCamera))
+            {
+                baseCameraData.cameraStack.Add(CommonUiRoot.Inst.UiCamera);
+            }
+            if (overlayCameraData.renderType != CameraRenderType.Overlay)
+            {
+                Debug.LogWarning("[CameraStackDebugger] Overlay カメラが Overlay タイプではありません。");
+            }
+            if (baseCameraData.cameraStack.Contains(CommonUiRoot.Inst.UiCamera))
+            {
+                Debug.Log("[CameraStackDebugger] UIカメラは Base カメラに正しくスタックされています。");
+            }
+            else
+            {
+                Debug.LogError("[CameraStackDebugger] UIカメラが Base カメラにスタックされていません。");
+            }
+        }
     }
 }
