ced69e0255aa0ce8fddb489f69cba586b83cceda
tr-t-mitsumaru <tricore.as03@gmail.com>
2026-01-30 10:06:41 +0900

    ビルドエラー回避のため、いったんSwitch2ではCPUブーストモード機能を塞いでおく

--- Scripts/Utility/GraphicSettingsApplyer.cs
+++ Scripts/Utility/GraphicSettingsApplyer.cs
@@ -97,7 +97,7 @@ public class GraphicSettingsApplyer
         ApplyFrameRate(0);
 #endif
         // ロード高速化のためにCPUブースト戻す
-#if (UNITY_SWITCH || UNITY_SWITCH2) && !UNITY_EDITOR  && !UNITY_STANDALONE_WIN
+#if (UNITY_SWITCH) && !UNITY_EDITOR  && !UNITY_STANDALONE_WIN
         UnityEngine.Switch.Performance.SetCpuBoostMode(UnityEngine.Switch.Performance.CpuBoostMode.Normal);
 #endif
         Application.backgroundLoadingPriority = ThreadPriority.Low;
@@ -216,7 +216,7 @@ public class GraphicSettingsApplyer
         ApplyFrameRate(0, LoadBoostingFrameRate);
 
         // ロード高速化のためにCPUブースト
-#if (UNITY_SWITCH || UNITY_SWITCH2) && !UNITY_EDITOR  && !UNITY_STANDALONE_WIN
+#if (UNITY_SWITCH) && !UNITY_EDITOR  && !UNITY_STANDALONE_WIN
         UnityEngine.Switch.Performance.SetCpuBoostMode(UnityEngine.Switch.Performance.CpuBoostMode.FastLoad);
 #endif
         Application.backgroundLoadingPriority = ThreadPriority.High;
