96823c20b73adf9713014090b87197b6dff2b319
tr-t-mitsumaru <tricore.as03@gmail.com>
2026-06-05 21:57:38 +0900

    Switch2における垂直同期とフレームレートの設定まわり修正

--- Scripts/Utility/GraphicSettingsApplyer.cs
+++ Scripts/Utility/GraphicSettingsApplyer.cs
@@ -100,7 +100,7 @@ public class GraphicSettingsApplyer
         ApplyFrameRate(SaveDataManager.Data.VSyncCount);
 #elif UNITY_SWITCH2
         // Switch2は60固定
-        ApplyFrameRate(0, 60);
+        ApplyFrameRate(1, 60);
 #else
         ApplyFrameRate(0);
 #endif
@@ -127,8 +127,6 @@ public class GraphicSettingsApplyer
         targetFrameRate = 60;
 #elif UNITY_GAMECORE_XBOXONE || UNITY_SWITCH || UNITY_PS4
         QualitySettings.vSyncCount = 2;
-#elif UNITY_SWITCH2
-        QualitySettings.vSyncCount = 0;
 #else
         QualitySettings.vSyncCount = 1;
 #endif
